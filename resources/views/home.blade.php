b06803aacf0630b4543f6d37a271c632cab9f93d
ValentinaAAC <valentina.aac@hotmail.com>
2020-11-04 00:51:18 -0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('Bienvenidoooooooo!') }}
                 </div>
             </div>
         </div>
