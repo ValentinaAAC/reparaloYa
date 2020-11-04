75ff0940214111910bd2bae69aea4e5b3e96e750
ValentinaAAC <valentina.aac@hotmail.com>
2020-11-04 02:53:13 -0300

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        //$this->middleware('auth');
     }
 
     /**
@@ -23,7 +23,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('index');
     }
 
     //es probable que aquí se pongan los roles; todo lo que pongamos aquí lo va a controlar --
