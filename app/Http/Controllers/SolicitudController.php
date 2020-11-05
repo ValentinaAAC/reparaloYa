761eb3989e81a4ee48940d3d83e04769fb458ff5
Davis2796 <73859411+Davis2796@users.noreply.github.com>
2020-11-05 20:07:44 -0300

    Update SolicitudController.php

--- app/Http/Controllers/SolicitudController.php
+++ app/Http/Controllers/SolicitudController.php
@@ -6,5 +6,20 @@ use Illuminate\Http\Request;
 
 class SolicitudController extends Controller
 {
-    //
+    public function _construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        return view('solicitudtecnico'); 
+    }
+
+    public function store(Request $request)
+    {
+        dd($request->all());
+    }
+
+
 }
