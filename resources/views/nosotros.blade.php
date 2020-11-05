337c56c1fe78be8a1b3ae575c017f6242b09863b
ValentinaAAC <valentina.aac@hotmail.com>
2020-11-05 18:41:10 -0300

    Create nosotros.blade.php

---
+++ resources/views/nosotros.blade.php
@@ -0,0 +1,224 @@
+@extends('layouts.base')
+@section('contenido')
+
+  <div class="about">
+    <div class="container">
+      <div class="text-center">
+        <h2>Quienes somos!</h2>
+        <div class="col-md-10 col-md-offset-1">
+          <p>Somos la compañía líder en delivery online de América Latina y transformamos el concepto de delivery. Ofrecemos una solución en el bolsillo de los usuarios.
+            Actualmente en la localidad de Chillán, se encuentran numerosos trabajadores independientes de reparación equipos informáticos, impresoras, Access Point electrodomesticos, linea blanca, climatizacion, gasfiteria y electricidad. que desean salir adelante, emprender y cumplir sus objetivos y metas.
+ Numerosas personas, al surgirles un problema en sus hogares de esta índole, ya sea por un factor interno o externo, no poseen la información de algún especialista al que puedan recurrir de manera rápida y segura, ya que en muchos casos las personas no tienen como asegurar que el técnico cuente con la capacitación adecuada para efectuar dicha labor.
+Otro inconveniente que surge es a la hora de pactar horarios o realizar la visita programada, ya que en varias ocasiones no se cumplen, desfavoreciendo al cliente de manera severa puesto que ya organizó una bitácora de su día contemplando lo acordado. Además, cabe destacar que algunos trabajadores independientes abusan de sus tarifas y son excesivamente altas en comparación al mercado actual.  
+Algunos técnicos no desempeñan sus servicios a domicilio, lo que perjudica a los clientes que no cuentan con algún tipo de vehículo o medio de transporte, por lo que se vuelve algo contra producente ya que las personas siempre optarán por el método más fácil y cómodo de resolver sus problemas. 
+
+Es por eso que creamos esta plataforma web para dar solucion a todos estos problemas. 
+En la aplicación web encuentras especialistas totalmente capacitados para resolver tus problemas.
+¡Vamos volando! llega el tecnico a la puerta de tu casa.</p>
+        </div>
+
+         <hr>
+      </div>
+    </div>
+  </div>
+  
+
+   <div class="team">
+        <div class="text-center wow fadeInDown">
+          <h2>Equipo fundador</h2>
+          <p>La clave del éxito esta en detectar hacia dónde va el mundo y llegar ahí primeros.</p>
+        </div>
+
+     <!-- equipo -->
+        <div class="row clearfix">
+          <div class="col-md-4 col-sm-6">
+            <div class="single-profile-top wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
+              <div class="media">
+                <div class="pull-left">
+                  <a href="#"><img class=" " src="img/man1.jpg" alt=""></a>
+                </div>
+                <div class="media-body">
+                  <h4>Enrique De La Fuente</h4>
+                  <h5>Programador Frontend</h5>
+                  <ul class="tag clearfix">
+                    <li class=""><a href="#"></a></li>
+                    <li class=""><a href="#"></a></li>
+                    <li class=""><a href="#"></a></li>
+                    <li class=""><a href="#"></a></li>
+                  </ul>
+
+                  <ul class="social_icons">
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+               
+                  </ul>
+                </div>
+              </div>
+              <!--/.media -->
+              
+            </div>
+          </div>
+ <!-------------------------------------------------------------------------------------------------------------------------->
+
+<div class="row clearfix">
+          <div class="col-md-4 col-sm-6">
+            <div class="single-profile-top wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
+              <div class="media">
+                <div class="pull-left">
+                  <a href="#"><img class=" " src="img/vale.jpg" alt=""></a>
+                </div>
+                <div class="media-body">
+                  <h4>Valentina Amaya</h4>
+                  <h5>Programador Laravel</h5>
+                  <ul class="tag clearfix">
+                    <li class=""><a href="#"></a></li>
+                    <li class=""><a href="#"></a></li>
+                    <li class=""><a href="#"></a></li>
+                    <li class=""><a href="#"></a></li>
+                  </ul>
+
+                  <ul class="social_icons">
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                   
+                  </ul>
+                </div>
+              </div>
+              <!--/.media -->
+              
+            </div>
+          </div>
+         
+
+<div class="row clearfix">
+          <div class="col-md-4 col-sm-6">
+            <div class="single-profile-top wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
+              <div class="media">
+                <div class="pull-left">
+                  <a href="#"><img class=" " src="img/man2.jpg" alt=""></a>
+                </div>
+                <div class="media-body">
+                  <h4>Benjamin Vasquez</h4>
+                  <h5>Base de datos </h5>
+                  <ul class="tag clearfix">
+                    <li class=""><a href="#"></a></li>
+                    <li class=""><a href="#"></a></li>
+                    <li class=""><a href="#"></a></li>
+                    <li class=""><a href="#"></a></li>
+                  </ul>
+
+                  <ul class="social_icons">
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    
+                  </ul>
+                </div>
+              </div>
+              <!--/.media -->
+              
+            </div>
+          </div>
+
+
+<div class="row team-bar">
+          <div class="first-one-arrow hidden-xs">
+            <hr>
+          </div>
+          <div class="first-arrow hidden-xs">
+            <hr> <i class="fa fa-angle-up"></i>
+          </div>
+          <div class="second-arrow hidden-xs">
+            <hr> <i class="fa fa-angle-down"></i>
+          </div>
+          <div class="third-arrow hidden-xs">
+            <hr> <i class="fa fa-angle-up"></i>
+          </div>
+          <div class="fourth-arrow hidden-xs">
+            <hr> <i class="fa fa-angle-down"></i>
+          </div>
+        </div>
+        <!--skill_border-->
+
+ <div class="col-md-2"> </div>
+ <div class="row clearfix">
+          <div class="col-md-4 col-sm-6">
+            <div class="single-profile-top wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
+              <div class="media">
+                <div class="pull-left">
+                  <a href="#"><img class=" " src="img/man3.jpg" alt=""></a>
+                </div>
+                <div class="media-body">
+                  <h4>Davis Bizama</h4>
+                  <h5>Base de datos</h5>
+                  <ul class="tag clearfix">
+                    <li class=""><a href="#"></a></li>
+                    <li class=""><a href="#"></a></li>
+                    <li class=""><a href="#"></a></li>
+                    <li class=""><a href="#"></a></li>
+                  </ul>
+
+                  <ul class="social_icons">
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                  
+                  </ul>
+                </div>
+              </div>
+              <!--/.media -->
+              
+
+            </div>
+          </div><div class="row clearfix">
+          <div class="col-md-4 col-sm-6">
+            <div class="single-profile-top wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
+              <div class="media">
+                <div class="pull-left">
+                  <a href="#"><img class=" " src="img/man4.jpg" alt=""></a>
+                </div>
+                <div class="media-body">
+                  <h4>Matias Guerrero </h4>
+                  <h5>Programador laravel</h5>
+                  <ul class="tag clearfix">
+                    <li class=""><a href="#"></a></li>
+                    <li class=""><a href="#"></a></li>
+                    <li class=""><a href="#"></a></li>
+                    <li class=""><a href="#"></a></li>
+                  </ul>
+
+                  <ul class="social_icons">
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    
+                  </ul>
+                </div>
+              </div>
+              <!--/.media -->
+              
+            </div>
+          </div>    
+
+ <!-------------------------------------------------------------------------------------------------------------------------->
+
+        </div>
+        <!--/.row-->
+      </div>
+      <!--section-->
+    </div>
+    <!--/.container-->
+  </section>
+              <!--/.media -->
+            
+            </div>
+          </div>
+      
+        </div>
+
+
+
+      </div>
+   
+    </div>
+
+  </section>
+
+@endsection
