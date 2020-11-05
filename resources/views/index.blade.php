e95ee763acfe7ca519c1bd22151d3f24c27dda15
ValentinaAAC <valentina.aac@hotmail.com>
2020-11-05 18:41:21 -0300

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,55 +1,8 @@
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
- 
-  <title>¡Repáralo Ya!</title>
-
-  <!-- Bootstrap -->
-  <link href="css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="css/font-awesome.min.css">
-  <link rel="stylesheet" href="css/animate.css">
-  <link href="css/animate.min.css" rel="stylesheet">
-  <link href="css/style.css" rel="stylesheet" />
-</head>
-
-<body>
-  <header id="header">
-    <nav class="navbar navbar-default navbar-static-top" role="banner">
-      <div class="container">
-        <div class="navbar-header">
-
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-          <div class="navbar-brand">
-            <a href="{{ url('/index') }}"><h1>¡Repáralo Ya!</h1></a>
-          </div>
-        </div>
-        <div class="navbar-collapse collapse">
-          <div class="menu">
-            <ul class="nav nav-tabs" role="tablist">
-              <li role="presentation"><a href="index.html" class="active">Home</a></li>
-                <li role="presentation"><a href="Servicios.html">Servicios</a></li>
-                <li role="presentation"><a href="">Nosotros</a></li>
-              <li role="presentation"><a href="{{ route('login') }}">{{ __('Iniciar sesión') }}</a></li>
-              <li role="presentation"><a href="{{ route('register') }}">{{ __('Únete a nuestro equipo') }}</a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </nav>
-  </header>
+@extends('layouts.base')
+@section('contenido')
 
 
-
+<!--Contenido-->
 <!--Slaider-->
   <div class="slider">
     <div id="about-slider">
@@ -205,42 +158,7 @@
   </footer>
 <!--/nadbar inferior-->
 
-<div class="sub-footer">
-    <div class="container">
-      <div class="social-icon">
-        <div class="col-md-4">
-          <ul class="social-network">
-            <li><a href="#" class="fb tool-tip" title="Facebook"><i class="fa fa-facebook"></i></a></li>
-            <li><a href="#" class="twitter tool-tip" title="Twitter"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="#" class="linkedin tool-tip" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
-     
-          </ul>
-        </div>
-      </div>
-
-      <div class="col-md-4 col-md-offset-4">
-        <div class="copyright">
-          &copy; ¡Repáralo Ya!. Todos los derechos reservados.
-          <div class="credits">
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-  <script src="js/jquery.js"></script>
-  <!-- Include all compiled plugins (below), or include individual files as needed -->
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/wow.min.js"></script>
-  <script>
-    wow = new WOW({}).init();
-  </script>
-</body>
-
-</html>
-
-
+@endsection
     
 
 
