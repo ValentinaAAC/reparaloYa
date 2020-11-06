76973ca1c6507b70a937a642111f5e6cc089a4d0
ValentinaAAC <valentina.aac@hotmail.com>
2020-11-06 01:32:45 -0300

    Create servicios.blade.php

---
+++ resources/views/servicios.blade.php
@@ -0,0 +1,104 @@
+@extends('layouts.base')
+@section('contenido')
+<div class="gallery">
+    <div class="text-center">
+      <h2>Nuestros servicios  </h2>
+      <p>La clave del éxito esta en detectar  hacia dónde va el mundo y llegar ahí primeros</p>
+    </div>
+    <div class="container">
+      <div class="col-md-4">
+        <figure class="effect-marley">
+          <img src="img/torre1.jpg" alt="" class="img-responsive" />
+          <figcaption>
+            <h4>Pc</h4>
+            <p>Limpieza, gestionde de cables, cambio de pasta termica, upgrade, formateo, istalacion de software etc.</p>
+          </figcaption>
+        </figure>
+      </div>
+      <div class="col-md-4">
+        <figure class="effect-marley">
+          <img src="img/ethernet.jpg" alt="" class="img-responsive" />
+          <figcaption>
+            <h4>Access point</h4>
+            <p>Instalacion de acces point, configuracion de router, levanytamiento de servicios grupo hogar etc.</p>
+          </figcaption>
+        </figure>
+      </div>
+      <div class="col-md-4">
+        <figure class="effect-marley">
+          <img src="img/Impresoras.jpg" alt="" class="img-responsive" />
+          <figcaption>
+            <h4>Impresoras y Scanner </h4>
+            <p>Mantencion, limpieza de inyectores y cabezales, cambio de almohadilla, hard reset etc.</p>
+          </figcaption>
+        </figure>
+      </div>
+    </div>
+
+ <div class="container">
+      <div class="col-md-4">
+        <figure class="effect-marley">
+          <img src="img/Gasfiteria.jpg" alt="" class="img-responsive" />
+          <figcaption>
+            <h4>Gasfiteria</h4>
+            <p>,Instalar, mantener y reparar las tuberías, Calefont, Fugas de Agua y Gas.etc.</p>
+          </figcaption>
+        </figure>
+      </div>
+      <div class="col-md-4">
+        <figure class="effect-marley">
+          <img src="img/electricidad.jpg" alt="" class="img-responsive" />
+          <figcaption>
+            <h4>Servicio de electricidad</h4>
+            <p>instalación de enchufes, lámparas; instalaciones de tableros eléctricos. Fallas eléctricas y reparaciones Cortocircuitos, Sobrecargas, Recableado  etc. </p>
+          </figcaption>
+        </figure>
+      </div>
+      <div class="col-md-4">
+        <figure class="effect-marley">
+          <img src="img/lineablanca.jpg" alt="" class="img-responsive" />
+          <figcaption>
+            <h4>Linea blanca</h4>
+            <p>Reparacion y mantencion de lavadora, resfrigerador, microondas etc.</p>
+          </figcaption>
+        </figure>
+      </div>
+    </div>
+
+
+
+     <div class="container">
+      <div class="col-md-4">
+        <figure class="effect-marley">
+          <img src="img/electrotv.jpg" alt="" class="img-responsive" />
+          <figcaption>
+            <h4>Electro</h4>
+            <p>Mantencion y reparacion de television, audio, HIFI.</p>
+          </figcaption>
+        </figure>
+      </div>
+      <div class="col-md-4">
+        <figure class="effect-marley">
+          <img src="img/climatizacion.jpg" alt="" class="img-responsive" />
+          <figcaption>
+            <h4>Climatizacion</h4>
+            <p>Instalacion, mantenimiento y programacion de equipo de climatizacion y aires acondicionado para el hogar.</p>
+          </figcaption>
+        </figure>
+      </div>
+     
+     <div class="col-md-4">
+        <figure class="effect-marley">
+          <img src="img/Proximamente.jpg" alt="" class="img-responsive" />
+          <figcaption>
+            <h4>Proximamente...</h4>
+            <p></p>
+          </figcaption>
+        </figure>
+      </div>
+    </div>
+
+
+  </div>
+
+  @endsection
