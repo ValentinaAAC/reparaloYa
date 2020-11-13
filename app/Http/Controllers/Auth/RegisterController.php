9bbc3bc9e07c7e6110ace6eef43e84e33bdb26f1
ValentinaAAC <valentina.aac@hotmail.com>
2020-11-12 21:34:24 -0300

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,6 +53,8 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'telephone' => ['required', 'string', 'max:20'],
+            'profesion' => ['required', 'string', 'max:200'],
         ]);
     }
 
@@ -68,6 +70,9 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'telephone' => $data['telephone'],
+            'profesion' => $data['profesion'],
+
         ]);
     }
 }
