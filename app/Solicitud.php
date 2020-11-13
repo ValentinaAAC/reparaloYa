b39e7f9db5cb188149c08d0ac140c35f8477fd88
Davis2796 <73859411+Davis2796@users.noreply.github.com>
2020-11-13 00:17:15 -0600

    Update Solicitud.php

--- app/Solicitud.php
+++ app/Solicitud.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Solicitud extends Model
 {
     protected $table = 'Solicitud';
-    protected $fillable = ['direccion','idServicio','problema'];
+    protected $fillable = ['nombre','direccion','telefono','idServicio','problema'];
 }
