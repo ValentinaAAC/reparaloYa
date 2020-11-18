4f79a0d0d5df3b0c62d2d80b93e549df12e77d70
ValentinaAAC <valentina.aac@hotmail.com>
2020-11-18 02:35:49 -0300

    Create usuario.blade.php

---
+++ resources/views/usuario.blade.php
@@ -0,0 +1,89 @@
+@extends('layouts.baseusuario')
+@section('contenido')
+
+<div class="container emp-profile">
+                <form method="post" enctype="multipart/form-data">
+                    <div class="row">
+                        <div class="col-md-4">
+                        <div class="profile-img">
+                            <img src="/img/perfil.jpg" alt=""/>
+                            <div class="file btn btn-lg btn-primary">
+                                Cambiar foto
+                                <input type="file" name="file"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="profile-head">
+                                    <h1>
+                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+                                    </h1>
+                                    <h6>
+                                        {{ Auth::user()->profesion }}
+                                    </h6>
+                                    <p class="proile-rating">RANKINGS : <span>8/10</span></p>
+                            <ul class="nav nav-tabs" id="myTab" role="tablist">
+                                <li class="nav-item">
+                                    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Acerca de</a>
+                                </li>
+                               
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <!-- boton editar perfil-->
+                <input type="submit"  class="profile-edit-btn" name="btnAddMore" value="Editar perfil"/>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                       
+                    </div>
+                    <div class="col-md-8">
+                        <div class="tab-content profile-tab" id="myTabContent">
+                            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Nombre</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>{{ Auth::user()->name }}</p>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Email</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>{{ Auth::user()->email }}</p>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Telefono</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>{{ Auth::user()->telephone }}</p>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Profesion</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>{{ Auth::user()->profesion }}</p>
+                                            </div>
+                                        </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>           
+        </div>
+
+
+
+
+@endsection
