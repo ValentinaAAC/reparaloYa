b9734f0e833e13425b0069a9fa9573391d495113
ValentinaAAC <valentina.aac@hotmail.com>
2020-11-21 17:03:58 -0300

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,6 +25,7 @@ $factory->define(User::class, function (Faker $faker) {
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'telephone' => $faker->telephone,
         'profesion' => $faker->profesion,
+        'image' => $faker->image,
         'remember_token' => Str::random(10),
     ];
 });
