e682a9192e5bd82a4e9607cf7b99d31a86d24929
ValentinaAAC <valentina.aac@hotmail.com>
2020-11-21 23:52:54 -0300

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <link rel="shortcut icon" href="images/logo.ico" />
     <title>RepáraloYa</title>
 
     <!-- Scripts -->
@@ -25,12 +26,11 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light shadow-sm" >
             <div class="container">
+            <img src="images/logo.png" width="47" height="47" class="d-inline-block align-top ml-4" alt="" loading="lazy"></a>
                 <a class="navbar-brand" href="{{ url('/index') }}">
                     <h2>¡REPÁRALO YA!</h2>
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+             
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
@@ -47,7 +47,7 @@
                             </li>
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('REGISTRARSE') }}</a>
+                                    <a class="nav-link" href="{{ env('APP_URL', '').'/registrousuario' }}">{{ __('REGISTRARSE') }}</a>
                                 </li>
                             @endif
                         @else
