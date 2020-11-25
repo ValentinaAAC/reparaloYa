cf848e1e510ad210eada4ca1c5feafe57ca2feb5
ValentinaAAC <valentina.aac@hotmail.com>
2020-11-25 17:22:10 -0300

    Update chatusuario.blade.php

--- resources/views/chatusuario.blade.php
+++ resources/views/chatusuario.blade.php
@@ -4,10 +4,10 @@
 <div class="container mt-3 my-3">
         
     <div class="row">
-        <div class="col-md-3">
+        <div class="col-md-6">
             @livewire("chat-form")
         </div>
-        <div class="col-md-3">
+        <div class="col-md-6">
             @livewire("chat-list")
         </div>
     </div>
