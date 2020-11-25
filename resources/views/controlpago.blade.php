fbb65f5f63304063b82116f54acbc45e22afdc76
ValentinaAAC <valentina.aac@hotmail.com>
2020-11-25 17:22:02 -0300

    Create controlpago.blade.php

---
+++ resources/views/controlpago.blade.php
@@ -0,0 +1,77 @@
+@extends('layouts.baseadmin')
+@section('contenido')
+
+
+<div class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="header">
+                                <h4 class="title">Reporte de membresia de especilistas</h4>
+                                <p class="category">visualiza el estado de cada especialista </p>
+                            </div>
+                            <div class="content table-responsive table-full-width">
+                                <table class="table table-hover table-striped">
+                                    <thead>
+                                        <th>ID</th>
+                                      <th>Nombre y apellido</th>
+                                      <th>Correo electronico</th>
+                                      <th>Telefono</th>
+                                      <th>Profesión</th>
+                                      <th>Estado</th>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                          <td>1</td>
+                                          <td></td>
+                                          <td></td>
+                                          <td></td>
+                                          <td></td>
+                                          <td>Activo</td>
+                                        </tr>
+                                       
+                                        <tr>
+                                            <td>2</td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td>Pago pendiente</td>
+                                        </tr>
+
+
+                                        <tr>
+                                            <td>3</td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                        </tr>
+                                        <tr>
+                                            <td>4</td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                        </tr>
+
+                                        
+
+                                    </tbody>
+                                    
+                                </table>
+
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        
+    </div>
+
+@endsection
