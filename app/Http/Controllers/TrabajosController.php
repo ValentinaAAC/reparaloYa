b5837166d33379dbf319fedf05facfa741c56ab8
ValentinaAAC <valentina.aac@hotmail.com>
2020-11-26 00:30:11 -0300

    Update TrabajosController.php

--- app/Http/Controllers/TrabajosController.php
+++ app/Http/Controllers/TrabajosController.php
@@ -4,14 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Solicitud;
+use App\Servicios;
+
+
+Use DB;
 
 
 class TrabajosController extends Controller
 {
     public function listado()
     {
-        $solicitud = Solicitud::all();
-        return view('trabajospublicados')->with(compact('solicitud'));
+        $solicitud = DB::table('solicitud')
+        ->join('servicios','solicitud.idServicios','=','servicios.idServicios')
+        ->select('solicitud.*','servicios.nombre')
+        ->get();
+        $servicios = Servicios::all();
+        return view('trabajospublicados',compact('solicitud','servicios'));
+        
     }
 
     
