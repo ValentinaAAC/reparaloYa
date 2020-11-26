83bd0b9de5e0dfa24bf20e6250e3bc42314215b5
ValentinaAAC <valentina.aac@hotmail.com>
2020-11-26 14:16:07 -0300

    Update historial.blade.php

--- resources/views/historial.blade.php
+++ resources/views/historial.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.basetrabajador')
 @section('contenido')
 
+
 <div class="content">
             <div class="container-fluid">
                 <div class="row">
@@ -11,25 +12,29 @@
                                 <p class="category">Aqui encontraras todos los detalles de tus trabajos aceptados</p>
                             </div>
                             <div class="content table-responsive table-full-width">
-                                <table class="table table-hover table-striped">
+                                <table class="table"  method="get">
                                     <thead>
                                         <th>Nº </th>
                                     	<th>Descripción del problema</th>
                                     	<th>Categoría</th>
-                                    	<th>Nombre</th>
                                     	<th>Dirección</th>
                                         <th>Teléfono</th>
                                     </thead>
+                                   
+                                    
                                     <tbody>
                                         <tr>
-                                        	<td>1</td>
-                                        	<td></td>
-                                        	<td></td>
-                                        	<td></td>
-                                        	<td></td>
+                                            <td>1</td>
+
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td>
                                         </tr>
                                     </tbody>
-                                    
+                                   
+                                   
                                 </table>
 
                             </div>
