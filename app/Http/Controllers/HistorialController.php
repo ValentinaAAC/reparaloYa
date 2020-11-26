8e928f850b8dfe9a939442608d5f6fbf8d962991
ValentinaAAC <valentina.aac@hotmail.com>
2020-11-26 00:30:26 -0300

    Update HistorialController.php

--- app/Http/Controllers/HistorialController.php
+++ app/Http/Controllers/HistorialController.php
@@ -4,12 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Solicitud;
+use App\Servicios;
+
+Use DB;
 
 class HistorialController extends Controller
 {
     public function listado()
     {
-        $solicitud = Solicitud::all();
-        return view('historialusuario')->with(compact('solicitud'));
+        $solicitud = DB::table('solicitud')
+        ->join('servicios','solicitud.idServicios','=','servicios.idServicios')
+        ->select('solicitud.*','servicios.nombre')
+        ->get();
+        $servicios = Servicios::all();
+        return view('historialusuario',compact('solicitud','servicios'));
     }
 }
