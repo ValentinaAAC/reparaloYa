a3b35256a88bdf94f2f7ca37099495b3bb9367fa
ValentinaAAC <valentina.aac@hotmail.com>
2020-11-26 01:35:58 -0300

    Update EditarPerfilController.php

--- app/Http/Controllers/EditarPerfilController.php
+++ app/Http/Controllers/EditarPerfilController.php
@@ -32,7 +32,7 @@ class EditarPerfilController extends Controller
          'profesion'=>$request['profesion'],
       ]);
 
-      return redirect()->route('home', $users);
+      return redirect()->route('usuario', $users);
 
    }
 }
