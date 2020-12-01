0f52fe3a21299b388b9546201cbe900ce7a72946
ValentinaAAC <valentina.aac@hotmail.com>
2020-12-01 03:41:39 -0300

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(UsersSeeder::class);
          $this->call(ServicioSeeder::class);
+         $this->call(UsersSeeder::class);
 
          
     }
