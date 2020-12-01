61644943896fba794e370a581b588bd6402a4ddb
ValentinaAAC <valentina.aac@hotmail.com>
2020-12-01 03:42:22 -0300

    Update User.php

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'telephone', 'profesion', 'image',
+        'name', 'email', 'password', 'telephone', 'idServicios', 'image',
     ];
 
     /**
