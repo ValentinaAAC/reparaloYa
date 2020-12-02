a3289ec82f32b90c194603028ae5e54733cb7121
matias guerrero <58151597+lakayser@users.noreply.github.com>
2020-12-02 19:17:57 -0300

    Update Chat.php

--- app/Chat.php
+++ app/Chat.php
@@ -1,13 +1,17 @@
 <?php
 
+
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Chat extends Model
 {
-    protected $table = "chat";
-    protected $fillable = [
-        "usuario", "mensaje"
-    ];
+    protected $guarded = [];
+
+    public function user(){
+
+        return $this->belongsTo(User::class);
+        
+    }
 }
