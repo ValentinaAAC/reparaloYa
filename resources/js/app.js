95daf86b6beef197e182971d1d67748007da7814
matias guerrero <58151597+lakayser@users.noreply.github.com>
2020-12-03 13:48:10 -0300

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,7 +5,7 @@ require('./bootstrap');
 window.Vue = require('vue');
 
  
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
 
 Vue.component('chat-component', require('./components/ChatComponent.vue').default);
 
