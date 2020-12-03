ac7db0233662c78e1813b415126c3f672a2367c0
matias guerrero <58151597+lakayser@users.noreply.github.com>
2020-12-03 13:48:44 -0300

    Update chatcolaborador.blade.php

--- resources/views/chatcolaborador.blade.php
+++ resources/views/chatcolaborador.blade.php
@@ -1,16 +1,8 @@
 @extends('layouts.basetrabajador')
 @section('contenido')
 
-<div class="container mt-3 my-3">
-
-    <div class="row">
-        <div class="col-md-6">
-            @livewire("chat-form")
-        </div>
-        <div class="col-md-6">
-            @livewire("chat-list")
-        </div>
-    </div>
+<div class="container">
+    <chat-component :user="{{ auth()->user() }}"></chat-component>
     
 </div>
 
