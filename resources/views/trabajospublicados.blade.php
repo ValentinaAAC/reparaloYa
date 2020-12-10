61de607c7d053a2084008e2c1807846b24ae3ddf
ValentinaAAC <valentina.aac@hotmail.com>
2020-12-10 13:39:15 -0300

    Update trabajospublicados.blade.php

--- resources/views/trabajospublicados.blade.php
+++ resources/views/trabajospublicados.blade.php
@@ -1,13 +1,6 @@
 @extends('layouts.basetrabajador')
 @section('contenido')
 
-<script>
-$(document).on('click', '.borrar', function (event) {
-    event.preventDefault();
-    $(this).closest('tr').remove();
-});
-</script>
-
 <div class="content">
 <div class="container-fluid">
 <table class="table" action="historial" method="get">
@@ -19,24 +12,43 @@ $(document).on('click', '.borrar', function (event) {
       <th scope="col">Nombre y Apellido</th>
       <th scope="col">Dirección</th>
       <th scope="col">Teléfono</th>
+      <th scope="col">Estado de solicitud</th>
     </tr>
   </thead>
   @foreach($solicitud as $solicitud)
   <tbody>
     <tr>
-    @if(Auth::user()->idServicios == $solicitud->idServicios)
+          @if(Auth::user()->idServicios == $solicitud->idServicios and Auth::user()->id==$solicitud->user_id)
           <td>{{ $solicitud->idSolicitud }}</td>
           <td>{{ $solicitud->problema }}</td>
           <td>{{ $solicitud->categoria }}</td>
           <td>{{ $solicitud->nombre }}</td>
           <td>{{ $solicitud->direccion }}</td>
           <td>{{ $solicitud->telefono }}</td>
-          <td>
-            <button class="btn btn-info" type="submit" name="aceptar" value ="Aceptar" >
-            <a href="{{ env('APP_URL', '').'/chatcolaborador' }}">{{ __('Aceptar') }}</a>
-            <input class="hidden" type="button" class="borrar btn btn-danger" />
+          <td>{{ $solicitud->estado }}</td>
+          @if($solicitud->estado=="pendiente")
+            <td>
+            <button type="submit" class="btn btn-success" name="aceptar" value="Aceptar" onClick="">
+            <a href="{{ action('TrabajosController@actualizarEst', [$solicitud->idSolicitud, 'aceptar']) }}" style="color:#198754;">Aceptar</a>
             </button>
+            </td>
+            <td> 
+            <button type="submit" class="btn btn-danger" name="rechazar" value="Rechazar" onClick="">
+            <a href="" style="color:#FF0000;">Rechazar</a>
+            </button>
+            </td>
+          @else
+            <td></td>
+            <td></td>
+            <td>
+            <a href="https://api.whatsapp.com/send?phone=[+56][{{$solicitud->telefono}}]&text=Hola,%20hablo%20con%20{{$solicitud->nombre}}?%20Me%20comunico%20con%20usted%20por%20su%20problema:%20<<{{$solicitud->problema}}>>." target="_blank"> 
+            <img class="alignnone wp-image-2180 ls-is-cached lazyloaded" src="https://sherpadigital.es/wp-content/uploads/2017/10/whatsapp-icon-150x150.png" data-src="https://sherpadigital.es/wp-content/uploads/2017/10/whatsapp-icon-150x150.png" alt="" data-srcset="https://i2.wp.com/sherpadigital.es/wp-content/uploads/2017/10/whatsapp-icon.png?zoom=2&amp;resize=97%2C107&amp;ssl=1 194w, https://i2.wp.com/sherpadigital.es/wp-content/uploads/2017/10/whatsapp-icon.png?zoom=3&amp;resize=97%2C107&amp;ssl=1 291w" sizes="(max-width: 77px) 100vw, 97px" srcset="https://i2.wp.com/sherpadigital.es/wp-content/uploads/2017/10/whatsapp-icon.png?zoom=2&amp;resize=97%2C107&amp;ssl=1 194w, https://i2.wp.com/sherpadigital.es/wp-content/uploads/2017/10/whatsapp-icon.png?zoom=3&amp;resize=97%2C107&amp;ssl=1 291w" width="47" height="47">
+            </a>
           </td>
+          @endif
+          <td></td>
+          
+        
     @endif
     </tr>
   </tbody>
