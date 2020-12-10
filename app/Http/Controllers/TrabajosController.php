cadab2628afe12258f6fdb0cf022da723246c76b
ValentinaAAC <valentina.aac@hotmail.com>
2020-12-10 13:37:58 -0300

    Update TrabajosController.php

--- app/Http/Controllers/TrabajosController.php
+++ app/Http/Controllers/TrabajosController.php
@@ -8,7 +8,9 @@ use App\Servicios;
 
 
 Use DB;
-
+Use Log;
+Use Redirect;
+Use Auth;
 
 class TrabajosController extends Controller
 {
@@ -16,11 +18,11 @@ class TrabajosController extends Controller
     {
         $solicitud = DB::table('solicitud')
         ->join('servicios','solicitud.idServicios','=','servicios.idServicios')
-        ->select('solicitud.*','servicios.categoria')
+        ->join('estados','solicitud.idSolicitud','=','estados.idSolicitud')
+        ->select('solicitud.*','servicios.categoria','estados.*')
         ->get();
         $servicios = Servicios::all();
         return view('trabajospublicados',compact('solicitud','servicios'));
-        
     }
 
     public function listadoSegui()
@@ -32,6 +34,20 @@ class TrabajosController extends Controller
         $servicios = Servicios::all();
         return view('seguimiento',compact('solicitud','servicios'));
         
+    }
+
+    public function actualizarEst($id, $estado)
+    {
+        if($estado="Aceptar"){
+            Log::debug($id."->".$estado);
+            $estado=DB::table('estados')->where('user_id',Auth::user()->id)->where('idSolicitud',$id)->update(array('estado' => 'Aceptada'));
+            DB::table('estados')->where('user_id', '!=' , Auth::user()->id)->delete();
+        }else{
+           Log::debug($id."->".$estado);
+           $estado=DB::table('estados')->where('user_id',Auth::user()->id)->where('idSolicitud',$id)->update(array('estado' => 'Rechazada'));
+           
+        }
+        return Redirect::to('/trabajospublicados');
     }
   
 }
